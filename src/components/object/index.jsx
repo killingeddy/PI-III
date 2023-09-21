66a641b6a85bf010b9bc94b455d2fc05d0b6448d
Eddie Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-09-21 15:43:59 -0300

    Models, gallery and some renders

--- src/components/object/index.jsx
+++ src/components/object/index.jsx
@@ -10,7 +10,7 @@ export default function Object({ model, objPosition, color, wPerspective, hPersp
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current, alpha: true });
-        renderer.setSize(wPerspective, hPerspective);
+        renderer.setSize(window.innerWidth, window.innerHeight);
 
         const controls = new OrbitControls(camera, renderer.domElement);
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,15 +4,15 @@ import React from 'react';
 export default function Home() {
 
   const [firstPosition, setFirstPosition] = React.useState('15, 3, 0');
-  const [secondPosition, setSecondPosition] = React.useState('-5, 0, 20');
+  const [secondPosition, setSecondPosition] = React.useState('0, 0, 3');
 
   return (
-    <div className="scrollbar-hide bg-cyan-900 relative w-screen h-screen">
+    <div className="scrollbar-hide relative w-screen h-screen">
       <div className="absolute bottom-0 right-0">
-      {/* <Object model={'whale'} objPosition={firstPosition} color={'0x61a5c2'} wPerspective={window.innerWidth} hPerspective={window.innerHeight} /> */}
+        {/* <Object model={'whale'} objPosition={firstPosition} color={'0x61a5c2'} wPerspective={window.innerWidth} hPerspective={window.innerHeight} /> */}
       </div>
       <div className="absolute top-0 left-0">
-      <Object model={'whale'} objPosition={secondPosition} color={'0x89c2d9'} wPerspective={600} hPerspective={300} />
+        <Object model={'octopus'} objPosition={secondPosition} color={'0xffffff'} />
       </div>
     </div>
   )
