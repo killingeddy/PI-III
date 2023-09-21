69cf370b436f3dc6e6165d59c3d232439feadf2a
Eddie Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-09-21 17:30:51 -0300

    Home inicialization

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,17 +3,11 @@ import React from 'react';
 
 export default function Home() {
 
-  const [firstPosition, setFirstPosition] = React.useState('15, 3, 0');
-  const [secondPosition, setSecondPosition] = React.useState('0, 0, 3');
+  const [secondPosition, setSecondPosition] = React.useState('-1, 0, -1');
 
   return (
-    <div className="scrollbar-hide relative w-screen h-screen">
-      <div className="absolute bottom-0 right-0">
-        {/* <Object model={'whale'} objPosition={firstPosition} color={'0x61a5c2'} wPerspective={window.innerWidth} hPerspective={window.innerHeight} /> */}
-      </div>
-      <div className="absolute top-0 left-0">
-        <Object model={'octopus'} objPosition={secondPosition} color={'0xffffff'} />
-      </div>
+    <div className="scrollbar-hide relative bg-sky-950 container">
+        <Object model={'diver'} objPosition={secondPosition} color={'0xffffff'} />
     </div>
   )
 }
