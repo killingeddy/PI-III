e512330bbba0aae14f1acce5c23fa854ab31176f
Eddie Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-09-22 13:44:26 -0300

    Sending audio to production

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,5 @@
+import ReactAudioPlayer from 'react-audio-player';
+import { FaHeadphonesAlt } from 'react-icons/fa';
 import Object from '@/components/object';
 import React from 'react';
 
@@ -5,9 +7,30 @@ export default function Home() {
 
   const [secondPosition, setSecondPosition] = React.useState('-1, 0, -1');
 
+  const [instructions, setInstructions] = React.useState('animate-fade');
+
+  React.useEffect(() => {
+    // setTimeout(() => {
+    //   setInstructions('animate-fadeout');
+    // }, 3500);
+  }, []);
+
   return (
-    <div className="scrollbar-hide relative bg-sky-950 container">
-        <Object model={'diver'} objPosition={secondPosition} color={'0xffffff'} />
+    <div className="scrollbar-hide relative bg-sky-950 container mx-auto h-screen overflow-hidden">
+      <div className="absolute top-20 left-1/3">
+        <Object model={'diver'} objPosition={secondPosition} color={'0xFFFF00'} />
+      </div>
+      <ReactAudioPlayer
+        src="/underwater.mp3"
+        autoPlay={true}
+        loop
+        volume={1}
+      />
+      <div className={`absolute bottom-20 left-1/2 transform -translate-x-1/2 justify-center flex-col items-center ${instructions} flex`}>
+        <FaHeadphonesAlt color='#ffffff95' size={40} />
+        <p className="text-white text-1xl opacity-80">Esse site usa áudio</p>
+        <p className="text-white text-1xl opacity-80">Use fones de ouvido para melhor experiência</p>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ module.exports = {
       colors: {
         primary: "#FF6363",
       },
+      keyframes: {
+        fade: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        fade: "fade 2s ease-in-out",
+        fadeout: "fade 2s ease-in-out reverse",
+      },
     },
   },
   plugins: [
